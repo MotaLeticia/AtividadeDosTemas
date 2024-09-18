de51f8013003628586c2925a1225b96f7ae2eebd
Leticia Mota <lfmota25@gmail.com>
2024-09-17 23:53:50 -0300

    Add files via upload

---
+++ Tema9/parOuImpar.py
@@ -0,0 +1,8 @@
+# Entrada do usuário
+numero = int(input("Digite um número inteiro: "))
+
+# Verifica se é par ou ímpar e exibe o resultado
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
