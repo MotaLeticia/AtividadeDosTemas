004f4aba30908082c8da5db2888d324d3a91284a
Leticia Mota <lfmota25@gmail.com>
2024-09-17 23:56:03 -0300

    Add files via upload

---
+++ Tema9/x_y.py
@@ -0,0 +1,11 @@
+# Entrada do usuário
+x = int(input("Digite o valor de x: "))
+y = int(input("Digite o valor de y: "))
+
+# Troca dos valores
+x, y = y, x
+
+# Exibe os novos valores
+print("Após a troca:")
+print("Valor de x:", x)
+print("Valor de y:", y)
